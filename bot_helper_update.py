2df6968338dff495b202c15138594684856424b1
God <sanyaatanasov@gmail.com>
2024-02-13 16:14:22 +0200

    main

--- bot_helper_update.py
+++ bot_helper_update.py
@@ -7,6 +7,9 @@ def parse_input(user_input):
     cmd = cmd.strip().lower()
     return cmd, *args
 
+
+
+
 def input_error(func):
     def inner(*args):
         try:
@@ -14,12 +17,15 @@ def input_error(func):
         except ValueError:
             return "Give me name and phone please."
         except KeyError:
-            return"Enter the argument for the command"
+            return"Помилка 😭"
         except IndexError:
             return"Enter the argument for the command"
         except Exception as eror:
             print(f"Error: {eror}")
     return inner
+
+
+
 
 @input_error
 def add_contact(*args):
@@ -30,6 +36,10 @@ def add_contact(*args):
 
 def show_all():
     print(list_contact)
+
+
+
+
 @input_error
 def change_contact(*args):
     update=False                    
@@ -42,6 +52,11 @@ def change_contact(*args):
     if update==False:
         print("Немає такого контакта")
 
+
+
+
+
+
 @input_error
 def show_phone(*args):
     update=False
@@ -53,6 +68,10 @@ def show_phone(*args):
     return ("Немає такого, перевірте та спробуйте ще раз" if update==False else "Знайденно")
     
 
+
+
+
+
 def main():
     os.system('cls')
     
@@ -62,28 +81,36 @@ def main():
         user_input = input("Enter a command: ")
         command, *args = parse_input(user_input)
         
+
         if command in ["close", "exit"]:
             print("Good bye!")
             break
 
+
         elif command == "hello":
             print("How can I help you?")
+
 
         elif command=="add":
             print(add_contact(*args))
         
+
         elif command=="change":     
             print(change_contact(*args))
                     
+
         elif command=="phone":
             print(show_phone(*args))
+
 
         elif command=="all":
             show_all()
 
+
         elif command in ["cls","clear"]:
             os.system('cls')
             
+
         else:
             print("Invalid command.")
 
